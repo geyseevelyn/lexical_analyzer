3664770704df753896a8d50422c5e295dacd5af4
FabricioDangellis <fabriciodangellisfdf@gmail.com>
2024-12-10 20:36:20 -0300

    feat: add function to manipulate symbol table

--- src/lexical_analyzer.py
+++ src/lexical_analyzer.py
@@ -31,44 +31,57 @@ token_count = {token: 0 for token in tokens}
 processed_tokens = [] 
 error_tokens = []
 
+# Adiciona o token à tabela de símbolos e atualiza o contador
+def add_to_symbol_table(token):
+    if any(entry['Valor'] == token.value for entry in symbol_table):
+        token_count[token.type] += 1
+        processed_tokens.append(token)
+        return  # Não adiciona duplicatas
+    symbol_table.append({
+        'Token': token.type,
+        'Valor': token.value
+    })
+    token_count[token.type] += 1
+    processed_tokens.append(token)
+
 # Expressões regulares para os tokens
 def t_KEYWORD(t):
     r'\b(some|all|value|min|max|exactly|that|not|and|or|only)\b|Class:|EquivalentTo:|Individuals:|SubClassOf:|DisjointClasses:'
     if t.value in keywords:
         t.type = 'KEYWORD'
-    #add_to_symbol_table(t)
+    add_to_symbol_table(t)
     return t
 
 def t_DATATYPE(t):
     r'(xsd|owl|rdf|rdfs):[a-zA-Z]+(?:[a-zA-Z0-9]*)?'
     if t.value in valid_datatypes:
-        #add_to_symbol_table(t)
+        add_to_symbol_table(t)
         return t
 
 def t_INDIVIDUAL_NAME(t):
     r'[A-Z][a-z]*[a-zA-Z]*[0-9]+'
-    #add_to_symbol_table(t)
+    add_to_symbol_table(t)
     return t
 
 def t_CLASS_ID(t):
     r'[A-Z][a-zA-Z]*(?:_[A-Z][a-zA-Z]*)*'
-    #add_to_symbol_table(t)
+    add_to_symbol_table(t)
     return t
 
 def t_PROPERTY_ID(t):
     r'(has[A-Z][a-zA-Z]*)|(is[A-Z][a-zA-Z]*Of)|([a-z][a-zA-Z]*)'
-    #add_to_symbol_table(t)
+    add_to_symbol_table(t)
     return t
 
 def t_SPECIAL_SYMBOL(t):
     r'[<>=\[\]{}(),"]|>=|<='
-    #add_to_symbol_table(t)
+    add_to_symbol_table(t)
     return t
 
 def t_CARDINALITY(t):
     r'-?\d+'
     t.value = int(t.value)
-    #add_to_symbol_table(t)
+    add_to_symbol_table(t)
     return t
 
 # Atualizar contagem de linhas
